577ac23c3bff9c51ff2e0ee3a7f50c62d4eb82d5
horlakz <officialhorlakz@gmail.com>
2023-10-20 19:25:40 +0100

    feat(/controller/): adds update profile endpoint to profile controller

--- src/main/java/com/shordem/blog/controller/ProfileController.java
+++ src/main/java/com/shordem/blog/controller/ProfileController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -86,5 +87,60 @@ public class ProfileController {
         profileService.saveProfile(profile);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(new MessageResponse("Profile created successfully!"));
+    }
+
+    @PatchMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> doUpdateProfile(@RequestParam(name = "firstName", required = false) String firstName,
+            @RequestParam(name = "lastName", required = false) String lastName,
+            @RequestParam(name = "bio", required = false) String bio,
+            @RequestParam(name = "website", required = false) String website,
+            @RequestParam(name = "mail", required = false) String mail,
+            @RequestParam(name = "x", required = false) String x,
+            @RequestParam(name = "facebook", required = false) String facebook,
+            @RequestParam(name = "instagram", required = false) String instagram,
+            @RequestParam(name = "avatar", required = false) MultipartFile file) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        Profile profile = profileService.findByUserId(user.getId())
+                .orElseThrow(() -> new UsernameNotFoundException("Profile Not Found with user id: " + user.getId()));
+
+        if (firstName != null) {
+            profile.setFirstName(firstName);
+        }
+        if (lastName != null) {
+            profile.setLastName(lastName);
+        }
+        if (bio != null) {
+            profile.setBio(bio);
+        }
+        if (website != null) {
+            profile.setWebsite(website);
+        }
+        if (mail != null) {
+            profile.setMail(mail);
+        }
+        if (x != null) {
+            profile.setX(x);
+        }
+        if (facebook != null) {
+            profile.setFacebook(facebook);
+        }
+        if (instagram != null) {
+            profile.setInstagram(instagram);
+        }
+        if (file != null) {
+            String fileKey = awsS3Service.uploadFile(file);
+            profile.setAvatar(fileKey);
+        }
+
+        profile.setUser(user);
+
+        profileService.saveProfile(profile);
+
+        return ResponseEntity.ok().body(new MessageResponse("Profile updated successfully!"));
+
     }
 }
