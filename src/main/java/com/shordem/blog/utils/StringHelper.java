ccfd0b83ca45f3d21692dcb1187d50bb931286c5
horlakz <officialhorlakz@gmail.com>
2023-12-06 21:50:09 +0100

    feat(/utils/): creates StringHelper class with static slugify method

---
+++ src/main/java/com/shordem/blog/utils/StringHelper.java
@@ -0,0 +1,22 @@
+package com.shordem.blog.utils;
+
+import java.text.Normalizer;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringHelper extends StringUtils {
+    private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
+    private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
+
+    public static String slugify(String text) {
+        String nowhitespace = WHITESPACE.matcher(text).replaceAll("-");
+        String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
+        String slug = NONLATIN.matcher(normalized).replaceAll("");
+        return slug.toLowerCase(Locale.ENGLISH);
+    }
+
+}
