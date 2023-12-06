cd4502ad09967fb3d917fa79834a95bdf3ba5073
horlakz <officialhorlakz@gmail.com>
2023-12-06 21:44:18 +0100

    feat(repository/): adds Repository stereotype directive  to UserRepository

--- src/main/java/com/shordem/blog/repository/UserRepository.java
+++ src/main/java/com/shordem/blog/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.shordem.blog.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.stereotype.Repository;
+
 import com.shordem.blog.entity.User;
 
+@Repository
 public interface UserRepository extends BaseRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
