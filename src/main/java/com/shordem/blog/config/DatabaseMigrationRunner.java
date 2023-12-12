1cdd8464643b3919e9719dd12392ac127085d531
horlakz <officialhorlakz@gmail.com>
2023-12-12 17:24:48 +0100

    enhancement(/config/): adds functionalities to add admin if admin doesn't exist

--- src/main/java/com/shordem/blog/config/DatabaseMigrationRunner.java
+++ src/main/java/com/shordem/blog/config/DatabaseMigrationRunner.java
@@ -1,9 +1,18 @@
 package com.shordem.blog.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import com.shordem.blog.entity.ERole;
+import com.shordem.blog.entity.Role;
+import com.shordem.blog.entity.User;
+import com.shordem.blog.service.UserService;
 
 @Component
 public class DatabaseMigrationRunner implements CommandLineRunner {
@@ -11,8 +20,27 @@ public class DatabaseMigrationRunner implements CommandLineRunner {
     @Autowired
     Flyway flyway;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
     @Override
     public void run(String... args) throws Exception {
+        if (!userService.findByUsername("admin").isPresent()) {
+            Set<Role> roles = new HashSet<>();
+
+            roles.add(new Role(ERole.ADMIN));
+
+            User user = new User();
+            user.setEmail("shordemmedia@gmail.com");
+            user.setUsername("admin");
+            user.setPassword(encoder.encode("password"));
+            user.setRoles(roles);
+
+            userService.saveUser(user);
+        }
         flyway.migrate();
     }
 
