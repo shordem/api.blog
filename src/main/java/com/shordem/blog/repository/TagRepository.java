1f5373719e4981e0472c1f1b2fcaed1702938a7d
horlakz <officialhorlakz@gmail.com>
2023-12-20 02:06:54 +0100

    Add methods to TagRepository.java

--- src/main/java/com/shordem/blog/repository/TagRepository.java
+++ src/main/java/com/shordem/blog/repository/TagRepository.java
@@ -1,6 +1,8 @@
 package com.shordem.blog.repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,8 @@ import com.shordem.blog.entity.Tag;
 public interface TagRepository extends JpaRepository<Tag, String> {
 
     Optional<Tag> findBySlug(String slug);
+
+    List<Tag> findAllByOrderByCreatedAtDesc();
+
+    Set<Tag> findAllBySlugIn(String[] tags);
 }
