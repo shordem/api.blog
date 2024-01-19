990753ada6e32db96e405964c4272dfdf0fd683c
horlakz <officialhorlakz@gmail.com>
2024-01-19 23:40:23 +0100

    enhancement(/repository/):Update PostRepository interface to use PagingAndSorting interface instead of JPARepository

--- src/main/java/com/shordem/blog/repository/PostRepository.java
+++ src/main/java/com/shordem/blog/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.shordem.blog.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import com.shordem.blog.entity.Post;
 
-public interface PostRepository extends BaseRepository<Post, UUID> {
+public interface PostRepository extends PagingAndSortingRepository<Post, UUID> {
 
     Optional<Post> findBySlug(String slug);
 
     boolean existsBySlug(String slug);
+
+    Post save(Post post);
 }
