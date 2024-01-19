a7fff2b7ee02b6708a2b2295250cdc7fb707de69
horlakz <officialhorlakz@gmail.com>
2024-01-19 23:40:55 +0100

    refactor(/service/):  PostService class and add createBlog method and pagination

--- src/main/java/com/shordem/blog/service/PostService.java
+++ src/main/java/com/shordem/blog/service/PostService.java
@@ -2,14 +2,20 @@ package com.shordem.blog.service;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.shordem.blog.dto.PostDto;
 import com.shordem.blog.dto.TagDto;
 import com.shordem.blog.entity.Post;
 import com.shordem.blog.entity.Tag;
+import com.shordem.blog.entity.User;
 import com.shordem.blog.exception.EntityNotFoundException;
+import com.shordem.blog.payload.request.PostRequest;
 import com.shordem.blog.repository.PostRepository;
 import com.shordem.blog.utils.StringHelper;
 
@@ -19,9 +25,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class PostService extends BaseService<Post> {
+public class PostService {
 
     private final PostRepository postRepository;
+    private final TagService tagService;
 
     private PostDto convertToDto(Post post) {
         Set<Tag> tags = post.getTags();
@@ -39,16 +46,29 @@ public class PostService extends BaseService<Post> {
         return slug;
     }
 
-    @Override
-    public void save(Post entity) {
-        entity.setSlug(createSlug(entity.getTitle()));
+    public void createBlog(PostRequest postRequest, User user) {
+        Post post = new Post();
 
-        postRepository.save(entity);
+        String title = postRequest.getTitle();
+        String content = postRequest.getContent();
+        String[] tags = postRequest.getTags();
+        String slug = createSlug(title);
+        Set<Tag> tagSet = tagService.findAllBySlugIn(tags);
+
+        post.setSlug(slug);
+        post.setTitle(title);
+        post.setContent(content);
+        post.setTags(tagSet);
+        post.setCreatedBy(user);
+
+        postRepository.save(post);
     }
 
-    public List<PostDto> findAll() {
-        return postRepository.findAll().stream().map(this::convertToDto)
-                .toList();
+    public Page<PostDto> findAll(Pageable pageable) {
+        List<Post> posts = postRepository.findAll(pageable).getContent();
+        List<PostDto> postDtos = posts.stream().map(this::convertToDto).collect(Collectors.toList());
+
+        return new PageImpl<>(postDtos, pageable, posts.size());
     }
 
     public PostDto findBySlug(String slug) {
