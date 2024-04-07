d2435be432a5719d4e46ba4d34211fd3f1d0fc24
horlakz <officialhorlakz@gmail.com>
2024-04-07 20:34:30 +0100

    feat(/config/): adds EmailConfig class to configuration classes

---
+++ src/main/java/com/shordem/blog/config/EmailConfig.java
@@ -0,0 +1,38 @@
+package com.shordem.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Component
+public class EmailConfig {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendMail(String to, String subject, String templateName, Context context) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+
+        String html = templateEngine.process(templateName, context);
+        helper.setText(html, true);
+
+        javaMailSender.send(mimeMessage);
+    }
+}
