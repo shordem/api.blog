8128c0d0321c70471015985c937fc06f2bf5db9d
horlakz <officialhorlakz@gmail.com>
2024-04-07 18:25:26 +0100

    Refactor EntityNotFoundAdvice to include exception message

--- src/main/java/com/shordem/blog/advice/EntityNotFoundAdvice.java
+++ src/main/java/com/shordem/blog/advice/EntityNotFoundAdvice.java
@@ -1,12 +1,11 @@
 package com.shordem.blog.advice;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.shordem.blog.exception.EntityNotFoundException;
 
@@ -15,7 +14,7 @@ public class EntityNotFoundAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
     ResponseEntity<Object> entityNotFoundHandler(EntityNotFoundException ex) {
-        Map<String, String> body = Map.of("message", "Record not found");
+        Map<String, String> body = Map.of("message", ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
