acdb8adf269dbe51450916a226ba8d69c3fa2ad5
horlakz <officialhorlakz@gmail.com>
2024-04-08 13:29:48 +0100

    feat(/service/): creates email service file for sending email

---
+++ src/main/java/com/shordem/blog/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.shordem.blog.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+public class EmailService {
+    private JavaMailSender javaMailSender;
+    private TemplateEngine templateEngine;
+    private String sender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender, TemplateEngine templateEngine, String sender) {
+        this.javaMailSender = javaMailSender;
+        this.templateEngine = templateEngine;
+        this.sender = sender;
+    }
+
+    public void sendMail(String to, String subject, String templateName, Context context) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setFrom(sender);
+
+        String html = templateEngine.process(templateName, context);
+        helper.setText(html, true);
+
+        javaMailSender.send(mimeMessage);
+    }
+}
